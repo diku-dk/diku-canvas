1514d6fdc07a038d3e62406fe640b716086f7ff1
Ken Friis Larsen <ken@friislarsen.net>
2022-09-10 14:27:01 +0200

    Fix showImg example

--- examples/showImg.fsx
+++ examples/showImg.fsx
@@ -5,7 +5,7 @@ let main =
         printfn "Please provide a file name"
     else
         let file = fsi.CommandLineArgs[1]
-        do printfn "Reading %A" file
         let canvas = Canvas.fromFile file
-        do printfn "height = %A width = %A" (Canvas.height canvas) (Canvas.width canvas)
-        Canvas.runApp file (Canvas.height canvas) (Canvas.width canvas) (fun _ _ _ -> canvas) (fun _ _ -> None) ()
+        let width = Canvas.width canvas
+        let height = Canvas.height canvas
+        Canvas.runApp file width height (fun _ _ _ -> canvas) (fun _ _ -> None) ()
