fa3dcb989840d0712075b49db0dcc13f55082b86
Ken Friis Larsen <ken@friislarsen.net>
2023-08-11 18:23:05 +0200

    Fix canvasNTLowlevelTest.fsx

--- examples/canvasNTLowlevelTest.fsx
+++ examples/canvasNTLowlevelTest.fsx
@@ -1,35 +1,42 @@
 #i "nuget:/Users/jrh630/repositories/diku-canvas/bin/Release/"
-#r "nuget:DIKU.Canvas, 2.0.0-alpha2"
+//#i "nuget:/Users/kfl/projects/fsharp-experiments/diku-canvas/bin/Release"
+#r "nuget:DIKU.Canvas, 2.0.0-alpha3"
 open Lowlevel
 
 #load "../canvasNT.fsx"
-open CanvasNT
+module CNT = CanvasNT
 
 type state = int // a counter
 
 let next x =
     x+1
 
+
+let pen = makePen green 5.0
+
+let drawCurve _ dc =
+    printfn "Curve";
+    drawLines pen [(10.0,10.0); (60.0, 80.0); (10.0, 80.0); (10.0, 10.0)] dc
+
+
 let mkDraw n = 
     match n with
         | "text" ->
             let txt = "Hello World"
-            let font = createFont "Microsoft Sans Serif" 36.0
-            let w,h = measureText font txt
-            let draw _ dc = textDC Color.White font "Hello World"
+            let font = CNT.createFont "Microsoft Sans Serif" 36.0
+            let w,h = CNT.measureText font txt
+            let draw _ dc = CNT.textDC white font "Hello World"
             w,h,(draw 0)
         | "curve" ->
-            let pen = new Pen(Color.White, 1f)
-            let draw _ dc = curveDC pen [(10.0,10.0); (20.0, 25.0); (10.0, 25.0); (10.0, 10.0)]
-            50,30,(draw 0)
+            100, 100, drawCurve
         | "combine" ->
             let txt = "Hello World"
-            let font = createFont "Microsoft Sans Serif" 36.0
-            let w,h = measureText font txt
-            let drawTxt _ dc = printfn "Txt"; textDC Color.White font "Hello World"
-            let pen = new Pen(Color.White, 1f)
-            let drawCurve _ dc = printfn "Curve"; curveDC pen [(10.0,10.0); (20.0, 25.0); (10.0, 25.0); (10.0, 10.0)]
-            (max 50 w),(max 30 h), drawCurve 0 >> drawTxt 0 
+            let font = CNT.createFont "Microsoft Sans Serif" 36.0
+            let w,h = CNT.measureText font txt
+            let drawTxt _ = printfn "Txt"; drawText "Hello World" font white 0f 0f
+            (max 100 w),(max 100 h), fun state dc ->
+                                         dc |> drawCurve state |> drawTxt state
+
         | _ -> failwith "Unkown test"
 
 let react (s:state) (ev:Lowlevel.Event) : state option =
@@ -43,7 +50,8 @@ let main args =
         1
     else
         let w, h, draw = mkDraw args[1]
-        runAppWithTimer "Text Test" (int w) (int h) (Some 0) draw react 0
+        runAppWithTimer "Text Test" (int w) (int h) None draw react 0
         0
+
 [<EntryPoint>]
 main fsi.CommandLineArgs
