efc34dac1842cdab61bb7a353571df8cc016284f
Jon Sporring <sporring@di.ku.dk>
2023-08-11 15:39:30 +0200

    choose test as command line argument

--- examples/canvasNTLowlevelTest.fsx
+++ examples/canvasNTLowlevelTest.fsx
@@ -10,19 +10,19 @@ type state = int // a counter
 let next x =
     x+1
 
-let TEST = 1
-let w,h,draw = 
-    match TEST with
-        | 0 ->
+let mkDraw n = 
+    match n with
+        | "text" ->
             let txt = "Hello World"
             let font = createFont "Microsoft Sans Serif" 36.0
             let w,h = measureText font txt
             let draw _ = textDC Color.White font "Hello World"
             w,h,fun dc -> draw 0
-        | _ ->
+        | "curve" ->
             let pen = new Pen(Color.White, 1f)
             let draw _ = curveDC pen [(10.0,10.0); (20.0, 25.0); (10.0, 25.0); (10.0, 10.0)]
             50,30,fun dc -> draw 0
+        | _ -> failwith "Unkown test"
 
 let react (s:state) (ev:Lowlevel.Event) : state option =
     match ev with
@@ -30,4 +30,13 @@ let react (s:state) (ev:Lowlevel.Event) : state option =
             s |> next |> Some
         | _ -> None
 
-runAppWithTimer "Text Test" (int w) (int h) (Some 0) draw react 0
+let main args =
+    printfn "%A" args
+    if Array.length args < 2 then
+        1
+    else
+        let w, h, draw = mkDraw args[1]
+        runAppWithTimer "Text Test" (int w) (int h) (Some 0) draw react 0
+        0
+[<EntryPoint>]
+main fsi.CommandLineArgs
