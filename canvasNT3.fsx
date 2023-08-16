425afb49fff8d38202f9b50e4e2a51accbe84ac9
Jon Sporring <sporring@di.ku.dk>
2023-08-16 06:50:05 +0200

    Updated interface for better tostring. Undid rotate superbox.
    Perhaps we should have a clipping function

--- canvasNT3.fsx
+++ canvasNT3.fsx
@@ -28,8 +28,8 @@ type Picture =
     | PiecewiseAffine of (pointF list)*Color*float*Size
     | Rectangle of Color*float*Size
     | Ellipse of Color*float*Size
-    | AlignH of Picture*Picture*Size
-    | AlignV of Picture*Picture*Size
+    | AlignH of Picture*Picture*float*Size
+    | AlignV of Picture*Picture*float*Size
     | OnTop of Picture*Picture*Size
     | Scale of Picture*float*float*Size
     | Rotate of Picture*float*float*float*Size
@@ -40,8 +40,8 @@ let getSize (p:Picture): Size =
     | PiecewiseAffine(_,_,_,sz)
     | Rectangle(_,_,sz)
     | Ellipse(_,_,sz)
-    | AlignH(_,_,sz)
-    | AlignV(_,_,sz)
+    | AlignH(_,_,_,sz)
+    | AlignV(_,_,_,sz)
     | OnTop(_,_,sz)
     | Scale(_,_,_,sz)
     | Rotate(_,_,_,_,sz)
@@ -51,15 +51,15 @@ let tostring (p:Picture): string =
     let rec loop (prefix:string) (p:Picture): string =
         let descentPrefix = (String.replicate prefix.Length " ")+"\u221F>"
         match p with
-            | PiecewiseAffine(points,c,sw,sz) -> prefix+(sprintf "PiecewiseAffine: %A - %A" (c,sw) points)
-            | Rectangle(c,sw,sz) -> prefix+(sprintf "Rectangle: %A - %A" (c,sw) sz)
-            | Ellipse(c,sw,sz) -> prefix+(sprintf "Ellipse: %A - %A" (c,sw) ((fst sz)/2.0,(snd sz)/2.0))
-            | AlignH(p1,p2,sz) -> sprintf "%sAlignH\n%s\n%s" prefix (loop descentPrefix p1) (loop descentPrefix p2)
-            | AlignV(p1,p2,sz) -> sprintf "%sAlignV\n%s\n%s" prefix (loop descentPrefix p1) (loop descentPrefix p2)
+            | PiecewiseAffine(points,c,sw,sz) -> sprintf "%sPiecewiseAffine (color,stroke)=%A coordinates=%A" prefix (c,sw) points
+            | Rectangle(c,sw,sz) -> sprintf "%sRectangle (color,stroke)=%A (width,height)=%A" prefix (c,sw) sz
+            | Ellipse(c,sw,sz) -> sprintf "%sEllipse (color,stroke)=%A (radiusX,radiusY)=%A" prefix (c,sw) ((fst sz)/2.0,(snd sz)/2.0)
+            | AlignH(p1,p2,pos,sz) -> sprintf "%sAlignH position=%g\n%s\n%s" prefix pos (loop descentPrefix p1) (loop descentPrefix p2)
+            | AlignV(p1,p2,pos,sz) -> sprintf "%sAlignV position=%g\n%s\n%s" prefix pos (loop descentPrefix p1) (loop descentPrefix p2)
             | OnTop(p1,p2,sz) -> sprintf "%sOnTop\n%s\n%s" prefix (loop descentPrefix p1) (loop descentPrefix p2)
-            | Scale(q,sx,sy,sz) -> sprintf "%sAffineTransform %g %g\n%s" prefix sx sy (loop descentPrefix q)
-            | Rotate(q,x,y,rad,sz) -> sprintf "%sRotate %g %g %g\n%s" prefix x y rad (loop descentPrefix q)
-            | Translate(q,dx,dy,sz) -> sprintf "%sTranslate %g %g\n%s" prefix dx dy (loop descentPrefix q)
+            | Scale(q,sx,sy,sz) -> sprintf "%sScale (scaleX,scaleY)=%A\n%s" prefix (sx, sy) (loop descentPrefix q)
+            | Rotate(q,x,y,rad,sz) -> sprintf "%sRotate (centerX,centerY)=%A radius=%g\n%s" prefix (x, y) rad (loop descentPrefix q)
+            | Translate(q,dx,dy,sz) -> sprintf "%sTranslate (dx,dy)=%A\n%s" prefix (dx, dy) (loop descentPrefix q)
     loop "" p
 
 /// Graphics primitives
@@ -95,7 +95,7 @@ let Center = 0.5
 let Bottom = 1.0
 let Right = 1.0
 
-let rec ontop (pic1:Picture) (pic2:Picture): Picture =
+let rec ontop (pic1:Picture) ((posX,posY): float*float) (pic2:Picture): Picture =
     let w1,h1 = getSize pic1
     let w2,h2 = getSize pic2
     let sz = (max w1 w2), (max h1 h2)
@@ -106,36 +106,23 @@ and alignh (pic1:Picture) (pos:float) (pic2:Picture): Picture =
     let w1,h1 = getSize pic1
     let w2,h2 = getSize pic2
     let sz = w1+w2, (max h1 h2)
-    let s = abs (pos*float (h1-h2))
-    if h1 > h2 then
-        AlignH(pic1, Translate(pic2, 0.0, s, (w2, h2+s)), sz)
-    elif h1 = h2 then
-        AlignH(pic1, pic2, sz)
-    else // something weird happens here, seems that clip removes the top of pic 2
-        AlignH(Translate(pic1, 0.0, s, (w1, h1+s)), pic2, sz)
+    AlignH(pic1,pic2,pos,sz)
 and alignv (pic1:Picture) (pos:float) (pic2:Picture): Picture =
     if pos < 0 || pos > 1 then 
         raise (System.ArgumentOutOfRangeException ("pos must be in [0,1]"))
     let w1,h1 = getSize pic1
     let w2,h2 = getSize pic2
     let sz = max w1 w2, h1 + h2
-    let s = abs (pos*float (w1-w2))
-    if h1 > h2 then
-        AlignV(pic1, Translate(pic2, s, 0.0, (w2+s, h2)), sz)
-    elif h1 = h2 then
-        AlignV(pic1, pic2, sz)
-    else 
-        AlignV(Translate(pic1, s, 0.0, (w1+s,h1)), pic2, sz)
+    AlignV(pic1, pic2, pos, sz)
 
 /// Drawing content
-let mutable i = 0
 let colorLst = [Color.Blue; Color.Cyan; Color.Green; Color.Magenta; Color.Orange; Color.Purple; Color.Yellow; Color.Red]
-let rec compile (expFlag: bool) (pic:Picture): PathTree =
+let rec compile (idx:int) (expFlag: bool) (pic:Picture): PathTree =
+    let next = (idx+1) % colorLst.Length
     let wrap sz expFlag dc =
         if expFlag then
-            let pen = solidPen colorLst[i] 1.0
+            let pen = solidPen colorLst[idx] 1.0
             let b = Prim (pen, Lowlevel.Rectangle(fst sz, snd sz))
-            i <- (i+1) % colorLst.Length
             dc <+> b
         else
             dc
@@ -152,52 +139,63 @@ let rec compile (expFlag: bool) (pic:Picture): PathTree =
         let dc = Prim (pen, Lowlevel.Ellipse(fst sz, snd sz))
         wrap sz expFlag dc
     | OnTop(p1, p2, sz) ->
-        let dc1 = compile expFlag p1
-        let dc2 = compile expFlag p2
+        let dc1 = compile next expFlag p1
+        let dc2 = compile next expFlag p2
         let dc = dc1 <+> dc2
         wrap sz expFlag dc
-    | AlignH(p1, p2, sz) ->
-        let w1, _ = getSize p1
-        let M = Matrix3x2.CreateTranslation(float32 w1,0f)
-        let dc1 = compile expFlag p1
-        let dc2 = transform M <| compile expFlag p2
-        let dc = dc1 <+> dc2
+    | AlignH(p1, p2, pos, sz) ->
+        let w1,h1 = getSize p1
+        let w2,h2 = getSize p2
+        let sz = w1+w2, (max h1 h2)
+        let s = float32 (abs (pos*float (h1-h2)))
+        let dc1 = compile next expFlag p1
+        let dc2 = compile next expFlag p2
+        let dc =
+            if h1 > h2 then
+                let M = Matrix3x2.CreateTranslation(float32 w1,s)
+                dc1 <+> (transform M <| dc2)
+            elif h1 = h2 then
+                dc1 <+> dc2
+            else
+                let M1 = Matrix3x2.CreateTranslation(0f,s)
+                let M2 = Matrix3x2.CreateTranslation(float32 w1,0f)
+                (transform M1 <| dc1) <+> (transform M2 <| dc2)
         wrap sz expFlag dc
-    | AlignV(p1, p2, sz) ->
-        let _,h1 = getSize p1
-        let M = Matrix3x2.CreateTranslation(0f,float32 h1)
-        let dc1 = compile expFlag p1 
-        let dc2 = transform M <| compile expFlag p2
-        let dc = dc1 <+> dc2
+    | AlignV(p1, p2, pos, sz) ->
+        let w1,h1 = getSize p1
+        let w2,h2 = getSize p2
+        let sz = max w1 w2, h1 + h2
+        let s = float32 (abs (pos*float (h1-h2)))
+        let dc1 = compile next expFlag p1 
+        let dc2 = compile next expFlag p2
+        let dc =
+            if w1 > w2 then
+                let M = Matrix3x2.CreateTranslation(s,float32 h1)
+                dc1 <+> (transform M <| dc2)
+            elif h1 = h2 then
+                dc1 <+> dc2
+            else 
+                let M1 = Matrix3x2.CreateTranslation(s,0f)
+                let M2 = Matrix3x2.CreateTranslation(0f,float32 h1)
+                (transform M1 <| dc1) <+> (transform M2 <| dc2)
         wrap sz expFlag dc
     | Translate(p, dx, dy, sz) ->
         let M = Matrix3x2.CreateTranslation(float32 dx,float32 dy)
-        let dc = transform M <| compile expFlag p
+        let dc = transform M <| compile next expFlag p
         wrap sz expFlag dc
     | Scale(p, sx, sy, sz) -> 
         let S = Matrix3x2.CreateScale(float32 sx, float32 sy)
-        let dc = transform S <| compile expFlag p
+        let dc = transform S <| compile next expFlag p
         wrap sz expFlag dc
     | Rotate(p, cx, cy, rad, sz) ->
         let R = Matrix3x2.CreateRotation(float32 rad)
         let T1 = Matrix3x2.CreateTranslation(float32 cx,float32 cy)
         let T2 = Matrix3x2.CreateTranslation(float32 -cx,float32 -cy)
         let M = T2*R*T1
-        let w,h = float32 (fst sz), float32 (snd sz)
-        let lst = 
-            [0f,0f; 0f, h; w, h; w, 0f]
-            |> List.map (fun (x,y) -> Vector2.Transform(Vector2(float32 x,float32 y),M))
-            |> List.map (fun v -> (float v.X,float v.Y))
-        let wMin, hMin = mapPairLst List.min lst 
-        let wMax, hMax = mapPairLst List.max lst
-        let ox,oy = (max 0.0 -wMin),(max 0.0 -hMin)
-        let T3 = Matrix3x2.CreateTranslation(float32 ox,float32 oy)
-        let dc = transform (M*T3) <| compile expFlag p
-        wrap (wMax+ox,hMax+oy) expFlag dc
+        let dc = transform M <| compile next expFlag p
+        wrap sz expFlag dc
 
 let make (p:Picture): drawing_fun = 
-    i<-0
-    compile false p |> drawPathTree
+    compile 0 false p |> drawPathTree
 let explain (p:Picture): drawing_fun = 
-    i<-0
-    compile true p |> drawPathTree
+    compile 0 true p |> drawPathTree
--- examples/canvasNT3Test.fsx
+++ examples/canvasNT3Test.fsx
@@ -7,18 +7,27 @@ open Lowlevel
 open CanvasNT3 // overwriting Kens Canvas functions!
 
 type state = int
-let lst = [(10.0,10.0); (60.0, 80.0); (10.0, 80.0); (10.0, 10.0)]
-let a = piecewiseaffine green 1.0 lst 
-let b = alignh a Center (scale 1.2 0.7 a)
-let c = alignv b Left (ellipse blue 2.0 20.0 25.0)
-let w,h = getSize c
-let d = rotate (w/2.0) (h/2.0) (20.0*System.Math.PI/180.0) c
-printfn "%s" (tostring d)
+let N = 360
+let next d = (d % N) + 1
 
-let draw _: drawing_fun = 
+let lst = [(10.0,10.0); (60.0, 80.0); (10.0, 80.0); (10.0, 10.0)]
+let tri1 = piecewiseaffine green 1.0 lst 
+let tri2 = scale 1.2 0.7 tri1
+let ell = ellipse blue 4.0 20.0 25.0
+let fig = alignv (alignh tri1 Center tri2) Left ell
+let fw,fh = getSize fig
+let w,h = 256,256
+
+let draw i: drawing_fun = 
+    let d = rotate (float fw/2.0) (float fh/2.0) ((float i)/(float (N-1))*2.0*System.Math.PI) fig
+    printfn "%s" (tostring d)
     explain d
 
-let react _ (ev:Lowlevel.Event) : state option =
-    None
+let react (s:state) (ev:Lowlevel.Event) : state option =
+    match ev with
+        | Event.TimerTick ->
+            s |> next |> Some
+        | _ -> None
 
-runAppWithTimer "CanvasNT2 Test" 256 256 None draw react 0
+runAppWithTimer "CanvasNT2 Test" w h (Some 100) draw react 0
+//runApp "CanvasNT2 Test" 256 256 draw
