65514c7701c5e9e3984b53142efd502928820b7e
Jon Sporring <sporring@di.ku.dk>
2023-08-27 16:54:35 +0200

    minor updates, major realization
    I realised that alignH and V must align as half-open intervals, since they are in floats. Eg., the right edge of box1 must be the same as the left edge of box2 in alignH. Otherwise there will always be space between them.

--- examples/sierpinski.fsx
+++ examples/sierpinski.fsx
@@ -1,22 +1,20 @@
-#i "nuget:/Users/jrh630/repositories/diku-canvas/bin/"
 #r "nuget:DIKU.Canvas, 2.0.0-alpha9"
 open Canvas
 open Color
 
-let w,l = 512,64 // size of window and base length of triangle
+let w,l = 512,64 // base length of triangle, and smalles triangle to draw
 
 // This program draws a Sierpinski triangle with bounding box length,length.
 let rec triangle (c:color) (minLength:float) (length:float): PrimitiveTree =
   if length <= minLength then // Smallest element to draw
     filledPolygon c [0.0,length; length,length; length/2.0,0.0; 0.0,length]
-  else // every triangle consists of 3 smaller trangles
+  else // every triangle consists of 3 smaller, identical triangles
     let subTriangle = triangle c minLength (length/2.0)
     alignV subTriangle Center (alignH subTriangle Center subTriangle)
 
-/// Prepare a Picture by the present state whenever needed
+/// Prepare a Picture
 let draw = 
-  let tree = 
-    triangle blue (float l) (float w)
+  let tree = triangle blue (float l) (float w)
   let ((x1,y1),(x2,y2)) = getBoundingBox tree
   translate -x1 y1 tree |> make
 
