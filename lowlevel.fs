0dcfe9ce5431aa39145b0bec74968416bed57568
Due <due@PoP.myguest.virtualbox.org>
2024-07-14 15:43:41 +0000

    Let us try this then.

--- lowlevel.fs
+++ lowlevel.fs
@@ -420,8 +420,8 @@ type Text(position: Vector2, text: string, color: Color, fontFamily: FontFamily,
     let mutable color = color
     let mutable size = size
     do
-        let pos = position - new Vector2(path.Bounds.Left, path.Bounds.Bottom)
-        path <- path.Translate(pos)
+        let pos = position - new Vector2(path.Bounds.Right, path.Bounds.Bottom)
+        path <- path.Translate(pos.X + path.Bounds.Width, pos.Y)
 
     static member FontFamilies = fontFamilies
 
@@ -468,8 +468,6 @@ type Text(position: Vector2, text: string, color: Color, fontFamily: FontFamily,
     
     member this.Position
         with get () = new Vector2(path.Bounds.Right, path.Bounds.Bottom)
-    member private this.Adjust
-        with get () = new Vector2(path.Bounds.Left, path.Bounds.Bottom)
     
     member this.Scale (scaling: Vector2) =
         path <- path.Scale(scaling.X, scaling.Y)
@@ -484,8 +482,8 @@ type Text(position: Vector2, text: string, color: Color, fontFamily: FontFamily,
         path <- path.Scale(scaling.X, scaling.Y)
     
     member this.SetPosition (newPosition: Vector2) =
-        let translation = newPosition - this.Adjust
-        path <- path.Translate(translation.X, translation.Y)
+        let translation = newPosition - this.Position
+        path <- path.Translate(translation.X + path.Bounds.Width, translation.Y)
 (*
 type Texture(position: Vector2, stream: IO.Stream) =
     let mutable image = Image.Load(stream)
